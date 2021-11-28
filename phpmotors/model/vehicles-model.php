673978f80b928b5590152044361c4f8ec6837c65
agneiting <gneitinga@byui.edu>
2021-11-27 22:00:23 -0700

    Activities for Week 11 all working

--- phpmotors/model/vehicles-model.php
+++ phpmotors/model/vehicles-model.php
@@ -151,4 +151,15 @@ function getVehicleDetails($invId) {
     return $vehicleDetails;
 }
 
+//Function that gets information for all vehicles
+function getVehicles(){
+	$db = phpmotorsConnect();
+	$sql = 'SELECT invId, invMake, invModel FROM inventory';
+	$stmt = $db->prepare($sql);
+	$stmt->execute();
+	$invInfo = $stmt->fetchAll(PDO::FETCH_ASSOC);
+	$stmt->closeCursor();
+	return $invInfo;
+}
+
 ?>
